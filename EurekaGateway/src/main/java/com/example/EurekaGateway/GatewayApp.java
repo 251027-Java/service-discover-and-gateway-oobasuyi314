df9e91a653dfb84112b3fb99b7e4c0c043aa4edf
“Osi” <oobasuyi@uncc.edu>
2025-12-17 10:47:07 -0600

    service and discover gateway

--- EurekaGateway/src/main/java/com/example/EurekaGateway/GatewayApp.java
+++ EurekaGateway/src/main/java/com/example/EurekaGateway/GatewayApp.java
@@ -2,10 +2,11 @@ package com.revature.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class GatewayApp {
 
     public static void main(String[] args) {
